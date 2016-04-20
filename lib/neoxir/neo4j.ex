e59f03ce0bc4048b3e89dc85eaaf63caeb0ab164
Jovanny Cruz <jovannypcg@yahoo.com>
2016-04-20 12:57:24 -0500

    feat(app): Add neo4j url

--- lib/neoxir/neo4j.ex
+++ lib/neoxir/neo4j.ex
@@ -1,6 +1,10 @@
-defmodule Neo4j do
+defmodule Neoxir.Neo4j do
+
+  @neo4j_url Application.get_env(:neoxir, :neo4j_url)
+
   @moduledoc """
   Provides functions to play with Neo4j, such as creating nodes, labes,
   relatiships and retrieve them.
   """
+
 end
