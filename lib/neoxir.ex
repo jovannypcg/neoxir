36177db7e856b161813261a2c73b63971f238cab
Jovanny Cruz <jovannypcg@yahoo.com>
2016-04-21 18:49:47 -0500

    feat(neoxir): Add functions to create and get nodes

--- lib/neoxir.ex
+++ lib/neoxir.ex
@@ -1,7 +1,7 @@
 defmodule Neoxir do
   @neo4j_url Application.get_env(:neoxir, :neo4j_url)
 
-  import Neoxir.Request, only: [headers: 0]
+  import Neoxir.Request
 
   @moduledoc """
   Provides functions to play with Neo4j, such as creating nodes, labes,
@@ -11,13 +11,46 @@ defmodule Neoxir do
   @doc """
   Creates a single node in Neo4j
   """
-  def create_single_node(properties \\ %{}) do
-    json_request = properties
-      |> Poison.encode
-      |> json_properties
+  def create_node(properties \\ %{}) do
+    {:ok, request_body } = Poison.encode(properties)
 
-    HTTPoison.post!(@neo4j_url <> "/db/data/node", json_request, headers)
+    HTTPoison.post!(@neo4j_url <> "/db/data/node", request_body, headers)
+      |> response_body
+      |> Poison.decode
+      |> poison_decoded_value
   end
 
-  defp json_properties({:ok, body}), do: body
+  @doc """
+  Creates a single node with a label in Neo4j
+  """
+  def create_node(properties, label) do
+    create_node(properties)
+      |> node_id_from_response
+      |> set_node_label(label)
+      |> get_node
+  end
+
+  @doc """
+  Gets a single node from the database
+  """
+  def get_node(node_id) do
+    url = @neo4j_url <> "/db/data/node/#{node_id}"
+
+    HTTPoison.get!(url, headers)
+      |> response_body
+      |> Poison.decode
+      |> poison_decoded_value
+  end
+
+  defp set_node_label(node_id, label) do
+    labels_url = @neo4j_url <> "/db/data/node/#{node_id}/labels"
+    {:ok, request_body } = Poison.encode(label)
+
+    HTTPoison.post!(labels_url, request_body, headers)
+      |> response_body
+
+    node_id
+  end
+
+  defp poison_decoded_value({ :ok, decoded_value }), do: decoded_value
 end
--- lib/neoxir/request.ex
+++ lib/neoxir/request.ex
@@ -13,6 +13,27 @@ defmodule Neoxir.Request do
       }
   end
 
+  @doc """
+  Returns the body of a POST response, whose status code is the specified
+  """
+  def response_body(%{status_code: 200, body: body}), do: body
+  def response_body(%{status_code: 201, body: body}), do: body
+  def response_body(%{status_code: 204, body: body}), do: body
+  def response_body(%{status_code: 404, body: _}) do
+    {:ok, encoded_value} = %{:error => "Object Not found"}
+      |> Poison.encode
+
+    encoded_value
+  end
+  def response_body(%{status_code: _, body: body}), do: { :error, body }
+
+  @doc """
+  Gets the ID of the node from a Neo4j response.
+  """
+  def node_id_from_response(body) do
+    body["metadata"]["id"]
+  end
+
   defp auth do
     encoded = @neo4j_user <> ":" <> @neo4j_pass
       |> Base.encode64
